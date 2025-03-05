61a4b3109c1b3dc1dc13092da6e1f33435669cd7
BruhNo13 <branocervenan123@gmail.com>
2025-03-05 19:50:25 +0100

    Upgraded algorithm for gaze detection, added fps counters + options to turn on/off boxes around face and eyes

--- Form1.cs
+++ Form1.cs
@@ -19,34 +19,24 @@ namespace GazeTrackingEmgu
 {
     public partial class Form1 : Form
     {
-        
-
         private Stopwatch gazeTimer = new Stopwatch();
         private string lastDirection = "Monitor";
         private VideoCapture _videoCapture;
         private CascadeClassifier _faceCascade;
         private CascadeClassifier _eyeCascade;
-        //private CascadeClassifier _profileFaceCascade;
-        //private CascadeClassifier _leftEyeCascade;
-        //private CascadeClassifier _rightEyeCascade;
-
+        private int gazeWarnings = 0; 
+        private int falseMonitorCount = 0;
+        private Stopwatch fpsTimer = new Stopwatch();
+        private int frameCount = 0;
+        private double fps = 0;
+        private bool showBoundingBoxes = true;
         private bool _isRunning = false;
 
         public Form1()
         {
             InitializeComponent();
-
             _faceCascade = new CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml");
             _eyeCascade = new CascadeClassifier("haarcascades/haarcascade_eye.xml");
-            // _profileFaceCascade = new CascadeClassifier("haarcascades/haarcascade_profileface.xml");
-            //_leftEyeCascade = new CascadeClassifier("haarcascades/haarcascade_lefteye_2splits.xml");
-            //_rightEyeCascade = new CascadeClassifier("haarcascades/haarcascade_righteye_2splits.xml");
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void startToolStripMenuItem_Click(object sender, EventArgs e)
@@ -100,7 +90,8 @@ namespace GazeTrackingEmgu
                     Rectangle face = faces.OrderByDescending(f => f.Width * f.Height).First();
                     if (face.Width < image.Width / 6) return;
 
-                    CvInvoke.Rectangle(image, face, new MCvScalar(0, 255, 0), 2);
+                    if (showBoundingBoxes)
+                        CvInvoke.Rectangle(image, face, new MCvScalar(0, 255, 0), 2);
 
                     Mat faceROI = new Mat(frame, face);
                     Rectangle[] eyes = _eyeCascade.DetectMultiScale(faceROI, 1.1, 4);
@@ -110,21 +101,34 @@ namespace GazeTrackingEmgu
                     Rectangle leftEye = sortedEyes[0].X < sortedEyes[1].X ? sortedEyes[0] : sortedEyes[1];
                     Rectangle rightEye = sortedEyes[0].X < sortedEyes[1].X ? sortedEyes[1] : sortedEyes[0];
 
-                    // **Zmenšíme výšku oblasti oka**
-                    int reducedHeight = (int)(leftEye.Height * 0.55); // Berieme iba spodnú polovicu oka
-                    leftEye = new Rectangle(face.X + leftEye.X, face.Y + leftEye.Y + reducedHeight / 2, leftEye.Width, reducedHeight);
-                    rightEye = new Rectangle(face.X + rightEye.X, face.Y + rightEye.Y + reducedHeight / 2, rightEye.Width, reducedHeight);
+                    int reducedHeight = (int)(leftEye.Height * 0.55); 
+                    int reducedWidth = (int)(leftEye.Width * 0.15);
+                    leftEye = new Rectangle(face.X + leftEye.X + reducedWidth, face.Y + leftEye.Y + reducedHeight / 2, leftEye.Width - (2 * reducedWidth), reducedHeight);
+                    rightEye = new Rectangle(face.X + rightEye.X + reducedWidth, face.Y + rightEye.Y + reducedHeight / 2, rightEye.Width - (2 * reducedWidth), reducedHeight);
 
-                    CvInvoke.Rectangle(image, leftEye, new MCvScalar(255, 0, 0), 2);
-                    CvInvoke.Rectangle(image, rightEye, new MCvScalar(255, 0, 0), 2);
-
-                    DrawEyeThresholdLines(image, leftEye);
-                    DrawEyeThresholdLines(image, rightEye);
+                    if (showBoundingBoxes)
+                    {
+                        CvInvoke.Rectangle(image, leftEye, new MCvScalar(255, 0, 0), 2);
+                        CvInvoke.Rectangle(image, rightEye, new MCvScalar(255, 0, 0), 2);
+                    }
 
                     Mat leftEyeROI = new Mat(frame, leftEye);
                     Mat rightEyeROI = new Mat(frame, rightEye);
                     DetectEyeDirection(image, leftEyeROI, leftEye);
                     DetectEyeDirection(image, rightEyeROI, rightEye);
+
+                    frameCount++;
+                    if (!fpsTimer.IsRunning)
+                        fpsTimer.Start();
+                    else if (fpsTimer.ElapsedMilliseconds >= 1000)
+                    {
+                        fps = frameCount / (fpsTimer.ElapsedMilliseconds / 1000.0);
+                        frameCount = 0;
+                        fpsTimer.Restart();
+                    }
+
+                    string fpsText = $"FPS: {fps:F1}";
+                    CvInvoke.PutText(image, fpsText, new Point(image.Width - 120, 30), FontFace.HersheySimplex, 0.7, new MCvScalar(0, 255, 0), 2);
 
                     pictureBox1.Invoke((MethodInvoker)(() =>
                     {
@@ -134,10 +138,9 @@ namespace GazeTrackingEmgu
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Chyba: " + ex.Message);
+                MessageBox.Show("Error: " + ex.Message);
             }
         }
-
 
         private void DetectEyeDirection(Image<Bgr, byte> image, Mat eyeROI, Rectangle eyeRect)
         {
@@ -148,10 +151,12 @@ namespace GazeTrackingEmgu
 
                 CvInvoke.EqualizeHist(grayEye, grayEye);
 
-                CvInvoke.GaussianBlur(grayEye, grayEye, new Size(5, 5), 0);
+                double minVal = 0, maxVal = 0;
+                Point minLoc = new Point(), maxLoc = new Point();
+                CvInvoke.MinMaxLoc(grayEye, ref minVal, ref maxVal, ref minLoc, ref maxLoc);
 
                 UMat thresholdEye = new UMat();
-                CvInvoke.Threshold(grayEye, thresholdEye, 50, 255, ThresholdType.BinaryInv);
+                CvInvoke.Threshold(grayEye, thresholdEye, (minVal + 30), 255, ThresholdType.BinaryInv);
 
                 using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
                 {
@@ -159,7 +164,7 @@ namespace GazeTrackingEmgu
 
                     if (contours.Size == 0)
                     {
-                        Console.WriteLine("Žiadne kontúry neboli nájdené!");
+                        Console.WriteLine("No contours found!");
                         return;
                     }
 
@@ -172,7 +177,7 @@ namespace GazeTrackingEmgu
                         double area = CvInvoke.ContourArea(contours[i]);
                         Rectangle tempRect = CvInvoke.BoundingRectangle(contours[i]);
 
-                        if (area > 50 && area < eyeROI.Width * eyeROI.Height * 0.4)
+                        if (area > 50 && area < eyeROI.Width * 0.25 * eyeROI.Height)
                         {
                             if (area > maxArea)
                             {
@@ -186,66 +191,107 @@ namespace GazeTrackingEmgu
                     if (maxIndex != -1)
                     {
                         Moments moments = CvInvoke.Moments(contours[maxIndex]);
+                        int centerX, centerY;
+
                         if (moments.M00 != 0)
                         {
-                            int centerX = (int)(moments.M10 / moments.M00);
-                            int centerY = (int)(moments.M01 / moments.M00);
+                            centerX = (int)(moments.M10 / moments.M00);
+                            centerY = (int)(moments.M01 / moments.M00);
+                        }
+                        else
+                        {
+                            centerX = boundingRect.X + boundingRect.Width / 2;
+                            centerY = boundingRect.Y + boundingRect.Height / 2;
+                        }
 
-                            CvInvoke.Circle(image, new Point(eyeRect.X + centerX, eyeRect.Y + centerY), 3, new MCvScalar(255, 0, 0), -1);
+                        if (centerX < boundingRect.X || centerX > boundingRect.X + boundingRect.Width)
+                        {
+                            centerX = boundingRect.X + boundingRect.Width / 2;
+                        }
+                        if (centerY < boundingRect.Y || centerY > boundingRect.Y + boundingRect.Height)
+                        {
+                            centerY = boundingRect.Y + boundingRect.Height / 2;
+                        }
 
-                            int eyeWidth = eyeROI.Width;
-                            int eyeHeight = eyeROI.Height;
-                            string direction = "Monitor";
+                        centerX += eyeRect.X;
+                        centerY += eyeRect.Y;
 
-                            double pupilShiftX = (double)centerX / eyeWidth;
-                            if (pupilShiftX < 0.3)
-                                direction = "Doprava";
-                            else if (pupilShiftX > 0.7)
-                                direction = "Do¾ava";
+                        if (showBoundingBoxes)
+                            CvInvoke.Circle(image, new Point(centerX, centerY), 3, new MCvScalar(255, 0, 0), -1);
 
-                            double pupilShiftY = (double)centerY / eyeHeight;
-                            if (pupilShiftY > 0.6)
-                                direction = "Dole";
-                            else if (pupilShiftY < 0.3)
-                                direction = "Hore";
+                        int eyeWidth = eyeROI.Width;
+                        int eyeHeight = eyeROI.Height;
+                        string direction = "Screen";
 
-                            Console.WriteLine($"Smer poh¾adu: {direction}");
+                        double pupilShiftX = (double)(centerX - eyeRect.X) / eyeWidth;
+                        if (pupilShiftX < 0.42) 
+                            direction = "Right";
+                        else if (pupilShiftX > 0.58) 
+                            direction = "Left";
 
+                        double pupilShiftY = (double)(centerY - eyeRect.Y) / eyeHeight;
+                        if (pupilShiftY > 0.75) 
+                            direction = "Down";
+                        else if (pupilShiftY < 0.25) 
+                            direction = "Up";
+
+                        Console.WriteLine($"Gaze direction: {direction}");
+
+
+                        if (direction != "Screen") 
+                        {
+                            if (!gazeTimer.IsRunning)
+                            {
+                                gazeTimer.Start();
+                                falseMonitorCount = 0; 
+                            }
+                            else if (gazeTimer.ElapsedMilliseconds > 3000) 
+                            {
+                                MessageBox.Show("Look at your screen!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                gazeTimer.Reset(); 
+                            }
+                        }
+                        else 
+                        {
+                            if (gazeTimer.IsRunning)
+                            {
+                                falseMonitorCount++; 
+
+                                if (falseMonitorCount >= 10) 
+                                {
+                                    gazeTimer.Reset(); 
+                                    falseMonitorCount = 0;
+                                }
+                            }
+                        }
+
+                        if (showBoundingBoxes)
+                        {
                             CvInvoke.Rectangle(image, eyeRect, new MCvScalar(0, 255, 0), 2);
 
-                            CvInvoke.Rectangle(image, new Rectangle(eyeRect.X + boundingRect.X, eyeRect.Y + boundingRect.Y, boundingRect.Width, boundingRect.Height), new MCvScalar(255, 0, 0), 2);
+                            int leftThreshold = eyeRect.X + (int)(eyeWidth * 0.42);
+                            int rightThreshold = eyeRect.X + (int)(eyeWidth * 0.58);
+                            int topThreshold = eyeRect.Y + (int)(eyeHeight * 0.25);
+                            int bottomThreshold = eyeRect.Y + (int)(eyeHeight * 0.75);
+
+                            CvInvoke.Line(image, new Point(leftThreshold, eyeRect.Y), new Point(leftThreshold, eyeRect.Y + eyeHeight), new MCvScalar(0, 0, 255), 2);
+                            CvInvoke.Line(image, new Point(rightThreshold, eyeRect.Y), new Point(rightThreshold, eyeRect.Y + eyeHeight), new MCvScalar(0, 0, 255), 2);
+                            CvInvoke.Line(image, new Point(eyeRect.X, topThreshold), new Point(eyeRect.X + eyeWidth, topThreshold), new MCvScalar(0, 0, 255), 2);
+                            CvInvoke.Line(image, new Point(eyeRect.X, bottomThreshold), new Point(eyeRect.X + eyeWidth, bottomThreshold), new MCvScalar(0, 0, 255), 2);
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Zrenica nebola nájdená v kontúrach!");
                     }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Chyba pri detekcii poh¾adu: " + ex.Message);
+                Console.WriteLine("Error while detecting gaze direction: " + ex.Message);
             }
         }
 
-
-
-
-
-        private void DrawEyeThresholdLines(Image<Bgr, byte> image, Rectangle eye)
+        private void toggleBoundingBoxesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            int leftThreshold = eye.X + (int)(eye.Width * 0.35);
-            int rightThreshold = eye.X + (int)(eye.Width * 0.65);
-            int topThreshold = eye.Y + (int)(eye.Height * 0.3);
-            int bottomThreshold = eye.Y + (int)(eye.Height * 0.6);
-
-            CvInvoke.Line(image, new Point(leftThreshold, eye.Y), new Point(leftThreshold, eye.Y + eye.Height), new MCvScalar(0, 0, 255), 2);
-            CvInvoke.Line(image, new Point(rightThreshold, eye.Y), new Point(rightThreshold, eye.Y + eye.Height), new MCvScalar(0, 0, 255), 2);
-            CvInvoke.Line(image, new Point(eye.X, topThreshold), new Point(eye.X + eye.Width, topThreshold), new MCvScalar(0, 0, 255), 2);
-            CvInvoke.Line(image, new Point(eye.X, bottomThreshold), new Point(eye.X + eye.Width, bottomThreshold), new MCvScalar(0, 0, 255), 2);
+            showBoundingBoxes = !showBoundingBoxes;
+            toggleBoundingBoxesToolStripMenuItem.Text = showBoundingBoxes ? "Hide boxes" : "Show boxes";
         }
-
-
-
     }
 }
